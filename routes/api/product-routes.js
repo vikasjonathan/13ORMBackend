e31c422ff4c834b6d7fe4190cb17530129138520
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-10 17:27:43 -0500

    Added routes get all get one

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,56 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      include: [{
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["id", "tag_name"],
+      },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+     where: {
+       id: req.params.id,
+     },
+     attributes: ["id", "product_name", "price", "stock", "category_id"],
+     include: [
+       {
+         model: Category,
+         attributes: ["id", "category_name"],
+       },
+       {
+         model: Tag,
+         attributes: ["id", "tag_name"],
+       },
+     ],
+   });
+       if (!productData) {
+         res.status(404).json({ message: "No product with that id" });
+         return;
+       }
+       res.status(200).json(productData);
+     } catch(err)  {
+       console.log(err);
+       res.status(500).json(err);
+     };
 });
 
 // create new product
