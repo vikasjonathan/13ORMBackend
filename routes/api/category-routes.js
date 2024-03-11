d2e1cf51bc46272521fac05d64220977b6ff596d
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-10 21:44:17 -0500

    Creating an category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,6 +54,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
