08075f8a69947ffbf7d11c35dc551da87fd10c17
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-10 21:47:48 -0500

    category update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -67,6 +67,21 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "No category with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
