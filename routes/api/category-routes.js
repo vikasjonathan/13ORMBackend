a3c656020e42c30da40adefac8b25e95c406d770
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-10 21:15:18 -0500

    Added get route for product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,23 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      
+      attributes: ["id", "category_name"],
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name", "price", "stock", "category_id"],
+        },
+      ],
+    });
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
